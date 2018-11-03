5a9727188cee1bdffa0aefd857b5db24b24a6394
aun <nattapad10@gmail.com>
2018-11-03 14:03:38 +0700

    4

--- lab8 Arrays and Collections/lab8 Arrays and Collections/Program.cs
+++ lab8 Arrays and Collections/lab8 Arrays and Collections/Program.cs
@@ -6,40 +6,24 @@ namespace lab8_Arrays_and_Collections
     {
         static void Main(string[] args)
         {
-            int[] data = new int[10];
-            //data[-1] = 0;
-            data[1] = 1;
-            data[9] = 9;
-            //data[10] = 10;
-            //Console.WriteLine(data[-1]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[9]);
-            //Console.WriteLine(data[10]);
-            Console.WriteLine("............................");
-            Console.WriteLine(data[7]);
-            Console.WriteLine("............................");
-            Console.WriteLine(data[4]+ data[9]);
-            Console.WriteLine("............................");
-            if (data[2]==data[6])
-                Console.WriteLine("data[2]=data[6]");
-            if (data[2] != data[6])
-                Console.WriteLine("data[2]=!data[6]");
-            Console.WriteLine("............................");
-            int i,n ;
-            n = 0;
-            for (i = 0; i < 10; i++)
+            int i;
+            double[] GPS = new double[9];
+            for (i = 0; i < 8; i++)
             {
-                n = n + data[i];
-                
+                Console.WriteLine("Plz input your GPS {0}", i + 1);
+                GPS[i] = double.Parse(Console.ReadLine());
+                GPS[8] = GPS[8] + GPS[i];
             }
-            Console.WriteLine("sum = {0}", i);
-            Console.WriteLine("............................");
-            for (i=0;i<10;i++)
-            {
-                Console.WriteLine("arry{0}={1}",i,data[i]);
-            }
-            Console.ReadLine();
 
+            for (i = 0; i < 8; i++)
+            {
+                Console.WriteLine("your GPS{0} = {1}", i + 1,GPS[i]);
+                if(i==7)
+                    Console.WriteLine("your GPA = {0}", GPS[8]/8);
+
+            }
         }
     }
 }
+
+
