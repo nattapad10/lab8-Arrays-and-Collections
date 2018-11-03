768288828211bf3e0115029955607f54718a95aa
aun <nattapad10@gmail.com>
2018-11-03 12:54:55 +0700

    3

--- lab8 Arrays and Collections/lab8 Arrays and Collections/Program.cs
+++ lab8 Arrays and Collections/lab8 Arrays and Collections/Program.cs
@@ -15,6 +15,29 @@ namespace lab8_Arrays_and_Collections
             Console.WriteLine(data[1]);
             Console.WriteLine(data[9]);
             //Console.WriteLine(data[10]);
+            Console.WriteLine("............................");
+            Console.WriteLine(data[7]);
+            Console.WriteLine("............................");
+            Console.WriteLine(data[4]+ data[9]);
+            Console.WriteLine("............................");
+            if (data[2]==data[6])
+                Console.WriteLine("data[2]=data[6]");
+            if (data[2] != data[6])
+                Console.WriteLine("data[2]=!data[6]");
+            Console.WriteLine("............................");
+            int i,n ;
+            n = 0;
+            for (i = 0; i < 10; i++)
+            {
+                n = n + data[i];
+                
+            }
+            Console.WriteLine("sum = {0}", i);
+            Console.WriteLine("............................");
+            for (i=0;i<10;i++)
+            {
+                Console.WriteLine("arry{0}={1}",i,data[i]);
+            }
             Console.ReadLine();
 
         }
