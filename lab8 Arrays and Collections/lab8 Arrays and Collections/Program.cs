bbf6a185078ac146b7feab68e4b93e386136af88
aun <nattapad10@gmail.com>
2018-11-05 04:34:13 +0700

    21

--- lab8 Arrays and Collections/lab8 Arrays and Collections/Program.cs
+++ lab8 Arrays and Collections/lab8 Arrays and Collections/Program.cs
@@ -8,29 +8,19 @@ namespace DictionaryDemo
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> dict1 = new Dictionary<string, int>();
-            dict1.Add("One", 1);
-            dict1.Add("Two", 2);
-            dict1.Add("Three", 3);
-            dict1.Add("Four", 4);
-            dict1.Add("Five", 5);
-            dict1.Add("Six", 6);
-            dict1.Add("Seven", 7);
-            dict1.Add("Eight", 8);
-            dict1.Add("Nine", 9);
-            dict1.Add("Ten", 10);
-
-            foreach (var item in dict1)
-            {
-                Console.WriteLine(item);
-            }
-
-            foreach (var item in dict1)
-            {
-                Console.WriteLine("key = {0}, value = {1}", item.Key, item.Value);
-            }
-
+            Dictionary<string, string> dict1 = new Dictionary<string, string>();
+            dict1.Add("TH", "Thailand");
+            
+         
+            
+                string value = dict1[Console.ReadLine()];
+                Console.WriteLine(value);
+            
+                
             Console.ReadLine();
         }
     }
 }
+
+
+
