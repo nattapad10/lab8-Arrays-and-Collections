e692ce1c986f6b3eecd16785f15aa27b92f20a91
aun <nattapad10@gmail.com>
2018-11-05 05:03:46 +0700

    22 The end  of LAB8

--- lab8 Arrays and Collections/lab8 Arrays and Collections/Program.cs
+++ lab8 Arrays and Collections/lab8 Arrays and Collections/Program.cs
@@ -1,22 +1,39 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 
-namespace DictionaryDemo
+namespace HashTableDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> dict1 = new Dictionary<string, string>();
-            dict1.Add("TH", "Thailand");
-            
-         
-            
-                string value = dict1[Console.ReadLine()];
-                Console.WriteLine(value);
-            
-                
+            Hashtable weeks = new Hashtable();
+            weeks.Add("60030029", "nattapad munkid");
+            weeks.Add("60030037", "Thanapon Yalan");
+            weeks.Add("60030014", "Jimmy");
+            weeks.Add("60030049", "nantikan tippawan");
+            weeks.Add("60030098", "sittinon tatiyajinda");            
+            weeks.Add("60030007", "kasina suksila");
+            weeks.Add("60030015", "jinnapat sangasang");
+            weeks.Add("60030112", "avirut meechoo");
+            weeks.Add("60030041", "thanakorn kodmongkol");
+            weeks.Add("60030031", "thanakamon intharasuriyawong");
+            weeks.Add("60030089", "sarun chokdurong");
+            weeks.Add("60030016", "jirawat chairongsri");
+
+            Console.WriteLine("---- all my friend ----");
+            foreach (DictionaryEntry day in weeks)
+            {
+                Console.WriteLine(day.Key + "   -   " + day.Value);
+            }
+            Console.WriteLine("---- plz input SID ----");
+            Console.WriteLine("" + weeks.ContainsKey(Console.ReadLine()));
+
+
+
+
+
+
             Console.ReadLine();
         }
     }
